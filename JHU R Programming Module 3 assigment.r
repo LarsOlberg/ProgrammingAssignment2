14a04c452aaf5251e059855016b5522c860edc72
LarsOlberg <67773125+LarsOlberg@users.noreply.github.com>
2020-09-06 07:19:53 +0200

    Create JHU R Programming Module 3 assigment.r

---
+++ JHU R Programming Module 3 assigment.r
@@ -0,0 +1,4 @@
+This assignment is beyond my competencies. Even though I tried hard, 
+even have difficulties understanding the two functions that are provided.
+Therefore, I submit this empty "solution" to be able to see how other 
+participants solved the assignment. 
